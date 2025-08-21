3a3d63b53876c7a305d15e409416b29467caecb1
rwr9857 <rwr9857@gmail.com>
2025-08-22 00:16:19 +0900

    Feat: admin 페이지 css 적용 및 미지정 url에 대해 리다이렉트

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -15,7 +15,7 @@ pip install -r requirements.txt
 ```bash
 python manage.py makemigrations
 python manage.py migrate
-gunicorn -c gunicorn.conf.py
+python manage.py runserver 0.0.0.0:8000
 ```
 
 # 도커 실행방법
--- codenova/settings.py
+++ codenova/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static', # 정적 파일 경로
 ]
