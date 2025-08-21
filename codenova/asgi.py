d600c278360478b32c8fa8a7e6d44dd741f727f1
rwr9857 <rwr9857@gmail.com>
2025-08-21 21:08:57 +0900

    Chore: blackNoise 설정

--- codenova/asgi.py
+++ codenova/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from blacknoise import BlackNoise
+from django.conf import settings
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codenova.settings')
 
 application = get_asgi_application()
+
+# blacknoise(asgi전용) 설정
+application = BlackNoise(get_asgi_application())
+application.add(settings.BASE_DIR / "static", "/static")
--- codenova/settings.py
+++ codenova/settings.py
@@ -133,12 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', # 정적 파일 경로
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static', # 정적 파일 경로
-]
