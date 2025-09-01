159641066601cb7ff1e59707d8d5bca424d1ddc7
daronus <isdjfv@gmail.com>
2025-09-01 15:31:57 +0900

    Feat : 콘플릭트 수정

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('main-chatbot/', views.main_chatbot_view, name='main-chatbot'),
+    path('api-chatbot/', views.api_chatbot_view, name='api-chatbot'),
     path('internal-docs/', views.internal_docs_view, name='internal-docs'),
-    path('community/', views.community_board_view, name='community-board'),
-    path('community/post/<int:post_id>/', views.post_detail_view, name='post-detail'),
+
+    path('api-chat/', include('apichat.urls')),
 ]
