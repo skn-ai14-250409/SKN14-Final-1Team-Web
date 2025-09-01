f89872d7f946610794fea176254da8b7bc507a95
jmy0913 <jmy09131@gmail.com>
2025-09-01 15:08:58 +0900

    Feat: api 챗봇 페이지 응답 백엔드 구현(기본 llm 사용 + retriever만 적용), gitignore에 chroma db 추가 #120

--- apichat/views.py
+++ apichat/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+
+import json
+import uuid
+import requests
+import os
+from datetime import datetime, timedelta, timezone
+
+from main.models import *
+from uauth.models import *
+from .utils.main import run_rag
 
 # Create your views here.
+
+@csrf_exempt
+# @login_required # 이후 로그인 기능 구현되면 주석 풀고 구현 필요
+def chat(request):
+    try:
+        data = json.loads(request.body)
+        user_message = data.get('message')
+        print(user_message)
+
+        response = run_rag(user_message)
+
+        response_data = {
+            'success': True,
+            'bot_message': response
+        }
+
+        return JsonResponse(response_data)
+
+    except Exception as e:
+        return JsonResponse({
+            'error': f'서버 오류가 발생했습니다: {str(e)}'
+        }, status=500)
+
+
+
+@csrf_exempt
+@login_required
+def get_chat_history(request, session_id):
+    pass
+
+
+@csrf_exempt
+@login_required
+def delete_session(request, session_id):
+    pass
+
+@csrf_exempt
+@login_required
+def chat_sessions(request):
+    pass
+
+@csrf_exempt
+@login_required
+def create_session(request):
+    """새 채팅 세션 생성"""
+    pass
+
+
+
+
