6090743c1e2d5f9a308328419d6f386bff30ac8c
jmy0913 <jmy09131@gmail.com>
2025-09-02 16:52:02 +0900

    Feat: 세션 생성 기능 추가 (세션 리스트만 추가되고 나오고, 로그인 유저의 세션 리스트를 가져와서 보여줌(아직 세션별 채팅 히스토리는 구현x)

--- apichat/urls.py
+++ apichat/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("chat/", views.chat, name="chat"),
+    path("api-chat/sessions/", views.session_list, name="session_list"),
     path("session_create/", views.create_session, name="session_create"),
 ]
