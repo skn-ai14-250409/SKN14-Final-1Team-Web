a4644f1e0023122b9f3bd02c0d23eebe0c794c35
jinijini20 <jinijini301301@gmail.com>
2025-09-02 09:47:37 +0900

    Feat: 로그인 ,회원가입프론트엔드,백엔드

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('api-chatbot/', views.api_chatbot_view, name='api-chatbot'),
     path('internal-docs/', views.internal_docs_view, name='internal-docs'),
+  
 ]
