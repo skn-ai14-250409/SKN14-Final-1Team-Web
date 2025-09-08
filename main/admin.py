02494a8b178d26d2536b33dea4c1a951ad095ebc
daronus <isdjfv@gmail.com>
2025-09-08 15:55:02 +0900

    Feat : 커뮤니티 페이지 (페이징 / 베스트 게시글,일반 게시글 분류 정상적으로 뜨게 함)

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "created_at", "views", "likes")
+    list_display_links = ("id", "title")
+    fields = ("title", "content", "author", "views", "likes")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "author", "created_at", "total_likes")
+
+
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
