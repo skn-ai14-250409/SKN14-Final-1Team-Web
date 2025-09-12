8ed8ca3e2e6b39097e6eece9ce7a21971ecf277d
rwr9857 <rwr9857@gmail.com>
2025-09-12 17:26:18 +0900

    CI: HTTPS 관련 설정 취소

--- codenova/settings_prod.py
+++ codenova/settings_prod.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = [
 
 # 웹 애플리케이션 서버가 리버스 프록시(Reverse Proxy)나 로드 밸런서(Load Balancer) 뒤에서 실행될 때
 # 클라이언트가 HTTPS를 통해 연결했음을 Django에 알리는 설정
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # 모든 HTTP 요청을 HTTPS로 강제 리디렉션(Redirection)시키는 설정
 # SECURE_SSL_REDIRECT = True
