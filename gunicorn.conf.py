2d91d8af7e3d1b6c5da628863b813de0514f19aa
jmy0913 <jmy09131@gmail.com>
2025-09-21 17:03:25 +0900

    Feat: gunicorn worker 1로 고정

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,43 +2,17 @@
 import multiprocessing
 
 
-# # workers 워커프로세스 개수
-# workers = multiprocessing.cpu_count() * 2 + 1
-# print("workers =", workers)
-#
-# # bind 주소/포트
-# bind = "0.0.0.0:8000"
-#
-# # worker_class 기본값:sync(동기워커)
-# worker_class = "uvicorn.workers.UvicornWorker"
-#
-# # wsgi_app 실행한 모듈 application
-# wsgi_app = "codenova.asgi:application"
+# workers 워커프로세스 개수
+workers = 1
+print("workers =", workers)
 
-
-
-# gunicorn.conf.py — 안전 기본값 (ASGI + 큰 모델)
+# bind 주소/포트
 bind = "0.0.0.0:8000"
 
-# 워커 2개로 줄임
-workers = 1
-
-# ASGI용 워커
+# worker_class 기본값:sync(동기워커)
 worker_class = "uvicorn.workers.UvicornWorker"
-
-# 타임아웃 여유 (첫 모델 로딩 보호)
-timeout = 300
-graceful_timeout = 30
-keepalive = 75
-
-# 메모리 누수 대비(선택)
-max_requests = 1000
-max_requests_jitter = 100
-
-# 로그(필요시)
-loglevel = "warning"
-accesslog = "-"
-errorlog = "-"
 
 # wsgi_app 실행한 모듈 application
 wsgi_app = "codenova.asgi:application"
+
+
